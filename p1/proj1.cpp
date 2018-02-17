a0e99ae15207ae9ff6828c243f0d89f73ba4fef4
Carlos Perez <carlosp_510@yahoo.com>
2018-02-16 19:02:09 -0800

    changed mbst base case to add edge to second adjList

--- p1/proj1.cpp
+++ p1/proj1.cpp
@@ -31,6 +31,8 @@ class edge
   int v_original;
 
 };
+void MBST( vector<list<edge> > &adjA,
+                vector<list<edge> > &adjD);
 int select(vector<int> &v, int start, int fin, int k);
 int main()
 {
@@ -54,14 +56,116 @@ int main()
     else
       cout << "Not a valid edge" << endl;
   }
-  //we have to collect weights into array A, in ascending order
+  //we have to collect weights into array A
+
   //we have to find median m of A->use select
   //collects edges with w<=m
   //Run BFS. is H connected? Yes, call camerini on H
-  //No, collapse G into g' and recursively call camerini on G'
+  //No, collapse G into G' and recursively call camerini on G'
+  //^^^^all this in MBST File
+
 
   return 0;
 }
+/*************************************************************/
+void MBST( vector<list<edge> > &adjA,
+                vector<list<edge> > &adjD){
+
+        //Count the number of edges in adjA
+        int m = 0;
+        int size = adjA.size();
+
+        for(int i = 0; i < size; i++){
+                m += adjA[i].size();
+        }
+        m = m >> 1;//divide by 2, each edge occurs twice in Adj
+        //BASE case, if number of edges is 1, return
+        //after adding the first edge to adjD
+        if(m == 1){
+            for(int u=0; u<size;u++)
+            {
+              if(adjA[u].size() != 0 )
+              {
+                list<edge>::iterator it=adjA[u].begin();
+                adjD[u].push_front(*it);//add edge(u,v)
+
+                int v=it->getV();
+                it=adjA[v].begin();
+                adjD[v].push_front(*it);//add edge(v,u)
+                break;
+              }
+            }
+
+        /***********   Collect weights of edges **********/
+        vector<int> weights(m, 0);
+        //You need to write this function:
+        collect_weights(adjA, weights);
+
+
+
+        /*******   Find the median of weights *********/
+        int k = m >> 1;//divide by 2
+        if(m % 2 == 0)
+                k--;
+        int median_weight = select(weights, 0, m-1,k);
+        //Function "select" is provided
+
+
+        /*****  Collect edges with weights <= median_weight
+                from adjA into adjB
+                Erase an edge from adjA:
+                it takes O(1) to erase (use erase) from
+                a list
+        *************************************************/
+        vector< list<edge> > adjB(adjA.size());//smallest edges
+
+        //You need to write this function:
+        select_smallest(adjA, adjB, median_weight);
+
+
+        /******* Find connected components in adjB
+                in linear time
+         ***************************************/
+        vector<short int> cc(adjB.size());
+        int total_cc = connected(adjB, cc);//number of comp
+
+
+
+        /**********     If connected (only one component)
+                we don't need edges of A anymore
+                just call recursively MBST and return
+                after the recursive call returns
+                i.e. do nothing after recursive call returns
+        ****************************************/
+
+        if(total_cc == 1){
+                MBST(adjB, adjD);
+                return;
+        }else{
+
+            /******     If not connected,
+                contract connected components
+                construct adjC from adjA and
+                super vertices (each represents a
+                connected component)
+
+                Rename edges according to the names
+                of new supervertices (0, 1,.., total_cc-1)
+                call recursively MBST on adjC:
+
+				MBST(adjC, adjD);
+
+                When call returns,
+                Add edges of adjB (use original names)
+                into adjD
+            *****************************************/
+
+                return;
+        }//else not connected
+}//MBST recursive
+
+
+/***********************************************************/
 
 int select(vector<int> &v, int start, int fin, int k){
 
