6cfce306ae1fdef2f32134b0dfcacc9d01d1ada1
Carlos Perez <carlosp_510@yahoo.com>
2018-03-12 14:16:32 -0700

    cleaned up proj3.cpp, made extra comments on certain code, final revision

--- p3/proj3.cpp
+++ p3/proj3.cpp
@@ -4,24 +4,43 @@
 #include <vector>
 using namespace std;
 
+void printB(const char *str,vector<int> B,int index);
+void minPartition(const char *str, int n);
+
+int main()
+{
+   // 1) reads string from standard input
+   string s;
+   cin >>s;
+   const char *str=s.c_str();
+   int size=s.size();
+   // 2) calculate table P,C, and B for backtracking
+   minPartition(str,size);
+   return 0;
+}
+
+/* prints substrings of palindrome partition */
 void printB(const char *str,vector<int> B,int index)
 {
-  //base case: if index >= 0
+  //base case: if index < 0
   if(index < 0){
     return;
   }
+  //recursively call printB until base case reached
+  //this will cause substrings to print from bottom up of
+  //vector B
   printB(str,B,B[index]-1);
-  for(int i=index; i>=B[index];--i)
+  for(int i=index; i>=B[index];i--)
   {
     cout << str[i];
   }
   cout << endl;
-
 }
-//calculates table P,C, and B for a given string and a size
+
+/*  calculates table P,C, and B for a given string and a size */
 void minPartition(const char *str, int n)
 {
-/***** You need three tables for this problem.*****/
+/************** You need three tables for this problem.*************/
 //C[i]=number of cuts in the best palindrome parition
 //B[i]=holds the index k for the best choice of C[i]
 //P[i][j]=truth table for either a palindrome or not
@@ -32,19 +51,14 @@ void minPartition(const char *str, int n)
   {
     P[i].resize(n);
   }
-  /*
-	int C[n];
-  int B[n];
-	bool P[n][n];
-  */
-/********* Calculate table P   ***************/
+/************** Calculate table P   ******************************/
 //P[i][j]=true if i=j
 	for (int i=0; i<n; i++)
 	{
 		P[i][i] = true;
 	}
 
-//L is the length of substring in character array
+//L is the length of a substring in character array
 	for (int L=2; L<=n; L++)
 	{
     int j;
@@ -62,7 +76,7 @@ void minPartition(const char *str, int n)
 		}
 	}
 
-/****** Calculate table C and B ******/
+/************** Calculate table C and B *************************/
 	for (int i=0; i<n; i++)
 	{
     //Condition 1: C[i]=0 if i=0
@@ -78,7 +92,7 @@ void minPartition(const char *str, int n)
     //Condition 3: C[i]=min(C[k] +1: p[k+1,i]=TRUE) if i>0
 		else
 		{
-			C[i] = INT_MAX;//macro that specifies that an integer variable cannot store any value beyond this
+			C[i] = INT_MAX;//macro, will be used as a sentinel value
 			for(int k=0;k<i;k++)
 			{
 				if(P[k+1][i] == true && C[k]+1<C[i]){
@@ -88,33 +102,6 @@ void minPartition(const char *str, int n)
 			}
 		}
 	}
-  /******  for debugging purposes, print tables  *******/
-  /*cout << "printing B and C: " << endl;
-  for(int c=0;c<n;c++)
-  {
-    cout << B[c] << " ";
-  }
-  cout << endl;
-  for(int c=0;c<n;c++)
-  {
-    cout << C[c] << " ";
-  }
-  cout << endl;
-  */
-	// print the min cut value for str[0..n-1]
+/********* Print susbstrings using string, B, and size *****/
   printB(str,B,n-1);
-	//cout << C[n-1] << endl;
-}
-
-int main()
-{
-   // 1) reads string from standard input
-   string s;
-   cin >>s;
-   const char *str=s.c_str();
-   int size=s.size();
-   // 2) calculate table P,C, and B for backtracking
-   minPartition(str,size);
-   //print out minimum number of cuts
-   return 0;
 }
