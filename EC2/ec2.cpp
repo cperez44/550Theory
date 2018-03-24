7d63861b592ab17a81369fef9c35d18a54c7865f
Carlos Perez <carlosp_510@yahoo.com>
2018-03-24 09:25:27 -0700

    added maxRev function, for test only

--- EC2/ec2.cpp
+++ EC2/ec2.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+//helper function that will return Maximum Revenue for given prices
+int maxRev(vector<int> prices,int size)
+{
+  for(int i=0;i<size;i++)
+  {
+    cout << "Test" << endl;
+  }
+
+}
+
 int main()
 {
-  int N,p;
+  int N,p;//N= size; p=price
   cin >> N;
-  for(int i=0;i<N;i++)
+  vector<int> prices(N);
+  for(int i=0;i<N;i++)//Note: i is not representative of land area
   {
     cin >>p;
-    
+    prices[i]=p;
   }
-  
+  //call maxRev here
+  maxRev(prices,N);
   return 0;
 }
