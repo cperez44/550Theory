a07ee9ebb0c1332cf20d59f667cd885e2ecdd7ca
Carlos Perez <carlosp_510@yahoo.com>
2018-03-24 10:41:46 -0700

    set up recursive function to generate maximum Revenue; compiles but not yet functional

--- EC2/ec2.cpp
+++ EC2/ec2.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
+#include <limits.h>
 #include <vector>
 
 using namespace std;
 
-//helper function that will return Maximum Revenue for given prices
-int maxRev(vector<int> prices,int size)
+//helper, recursive function that will calculate revenue table for given prices
+void maxRev(vector<int> prices,vector<int> &revenue,int n)
 {
-  for(int i=0;i<size;i++)
-  {
-    cout << "Test" << endl;
-  }
-
+  if(n>prices.size())//base case
+    return;
+  //wll compare to unreachable number,
+  int max_val=-1;
+  //loop through revenue table
+  //compare current max value with revenue up to n-i-1
+  //set revenue at n to whichever comparison decides is bigger
+  //recurse until base case reached
+  //maxRev(prices,revenue,n+1);
 }
 
 int main()
 {
   int N,p;//N= size; p=price
   cin >> N;
-  vector<int> prices(N);
+  vector<int> prices(N);//array of prices
+  vector<int> revenue(N);//array to keep track of revenue
+  revenue[0]=0;
   for(int i=0;i<N;i++)//Note: i is not representative of land area
   {
     cin >>p;
     prices[i]=p;
   }
   //call maxRev here
-  maxRev(prices,N);
+  maxRev(prices,revenue,1);
+  cout << revenue[N] << endl;
   return 0;
 }
