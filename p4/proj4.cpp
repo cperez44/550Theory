8342403d83f61de6d0a88deb9c0b50e19bbd73bd
Carlos Perez <carlosp_510@yahoo.com>
2018-04-17 16:23:43 -0700

    finished step 1: reading input file and concatinating to string T

--- p4/proj4.cpp
+++ p4/proj4.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <fstream>//ifstream
+#include <sstream>//stringstream
 using namespace std;
 
 int main()
 {
+  /*******  read in input file using cin  *******/
+  string filename;
+  ifstream file;
+  cout << "please specify input file: ";
+  cin >> filename;
+  cout << endl;
+  file.open(filename.c_str());
+
+  string T;
+  if(file.is_open())
+  {
+    stringstream ss;
+    string line;
+    while( getline( file, line) )
+    {
+      ss << line;
+    }
+    T=ss.str();
+  }
+  cout << T << endl;
 
 
+
+
+
+
+
+  file.close();
   return 0;
 }
